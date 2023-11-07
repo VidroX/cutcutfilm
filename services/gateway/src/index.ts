f75ed6217bd7e9fb26d334e17ccb18e5da97fb24
Vadym Karachenko <v1dr00x@gmail.com>
2023-11-07 00:15:43 -0800

    Add CORS and update dev/production apollo server plugins

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -2,8 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import http from 'http';
 import path from 'path';
-import { watch } from 'fs';
-import { promises as fs } from 'fs';
+import { readFileSync } from 'fs';
 import { ApolloGateway } from '@apollo/gateway';
 import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
@@ -16,6 +15,7 @@ import {
 	ApolloServerPluginLandingPageDisabled,
 	ApolloServerPluginUsageReportingDisabled,
 } from '@apollo/server/plugin/disabled';
+import { ApolloServerPluginInlineTrace } from '@apollo/server/plugin/inlineTrace';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,6 +26,7 @@ dotenv.config({
 
 const port = process.env.PORT ?? 4000;
 const debug = (process.env.DEBUG ?? 'false') === 'true';
+const nodeEnv = process.env.NODE_ENV ?? 'production';
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -34,50 +35,60 @@ const supergraphPath = path.join(__dirname, '..', 'supergraph.graphql');
 
 const gateway = new ApolloGateway({
 	debug,
-	async supergraphSdl({ update, healthCheck }) {
-		const watcher = watch(supergraphPath);
-
-		watcher.on('change', async () => {
-			try {
-				const updatedSupergraph = await fs.readFile(supergraphPath, 'utf-8');
-
-				await healthCheck(updatedSupergraph);
-
-				update(updatedSupergraph);
-			} catch (e) {
-				console.error(e);
-			}
-		});
-
-		return {
-			supergraphSdl: await fs.readFile(supergraphPath, 'utf-8'),
-
-			async cleanup() {
-				watcher.close();
-			},
-		};
-	},
+	supergraphSdl: readFileSync(supergraphPath).toString(),
 });
+
+const debugPlugins = debug
+	? [
+			ApolloServerPluginInlineTrace({
+				includeErrors: { transform: (err) => err },
+			}),
+	  ]
+	: [
+			ApolloServerPluginUsageReportingDisabled(),
+			ApolloServerPluginInlineTraceDisabled(),
+			ApolloServerPluginLandingPageDisabled(),
+	  ];
 
 const server = new ApolloServer({
 	gateway,
+	nodeEnv,
 	includeStacktraceInErrorResponses: debug,
-	plugins: [
-		ApolloServerPluginLandingPageDisabled(),
-		ApolloServerPluginUsageReportingDisabled(),
-		ApolloServerPluginInlineTraceDisabled(),
-		ApolloServerPluginDrainHttpServer({ httpServer }),
-	],
+	introspection: debug,
+	plugins: [...debugPlugins, ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 await server.start();
 
+const allowedOrigins = [
+	`http://localhost:${port}`,
+	'http://localhost',
+	'https://ccf-dev.vidrox.me',
+	'https://cutcutfilm.com',
+];
+
 app.use(
 	'/gql',
-	cors(),
+	cors({
+		origin: function (origin, callback) {
+			if (!origin) {
+				return callback(null, true);
+			}
+
+			if (allowedOrigins.indexOf(origin) === -1) {
+				const msg = 'The CORS policy does not allow access from the specified Origin.';
+
+				return callback(new Error(msg), false);
+			}
+
+			return callback(null, true);
+		},
+	}),
 	bodyParser.json({ limit: '10mb' }),
 	expressMiddleware(server, {
-		context: async ({ req }) => ({ token: req.headers.token }),
+		context: async ({ req }) => ({
+			token: (req.headers.authorization ?? '').trim().replace('Bearer ', ''),
+		}),
 	})
 );
 
